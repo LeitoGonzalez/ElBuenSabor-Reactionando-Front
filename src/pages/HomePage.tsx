fcfcf116c9f71760836c07865fb3cb4f9106e647
MaaR120 <martineguren120@gmail.com>
2023-11-12 11:54:03 -0300

    mergemartinfranco

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
-
-const HomePage = () => {
-  return (
-    <></>
-  )
-}
-=======
 const HomePage = () => {
   return <> </>;
 };
->>>>>>> 0fde1b738248a85a10c54972fe788d185b9aa476
 
-export default HomePage
+export default HomePage;
