25a062c82031ddc40114eb50f92b8530dd6c3eb1
MaaR120 <martineguren120@gmail.com>
2023-11-20 15:37:53 -0300

    preciocarritoarreglado

--- src/components/ProductosList/ProductosList.tsx
+++ src/components/ProductosList/ProductosList.tsx
@@ -1,11 +1,8 @@
 import { useEffect, useState } from "react";
 import { ProductoService } from "../../services/ProductoService";
-import { Producto } from "../../types/Producto";
 import { Button, Container, Table } from "react-bootstrap";
 import { TypeDetalleCarrito } from "../../types/TypeDetalleCarrito";
-
 import { DTOProducto } from "../../types/DTOProducto";
-
 
 /* import { TypeDetalleCarrito } from "../../types/TypeDetalleCarrito"; */
 type ProductListProp={
@@ -20,16 +17,15 @@ type ProductListProp={
 const ProductosList = ({detalleProducto,setDetalleProducto,total,setTotal,countProducts,setCountProducts}:ProductListProp) => {
 
   //useState lista de productos
-  const [productos, setProductos] = useState<Producto<"COCINA" | "BEBIDA">[]>();
+  const [productos, setProductos] = useState<DTOProducto[]>();
 
 
   const handleClick = (producto: DTOProducto) => {
 
-
     const detalleProductoItem : TypeDetalleCarrito ={
       cantidad: 1,
-      precioVenta: producto.precioVenta,
-      subTotal: producto.precioVenta,
+      precioVenta: producto.precio,
+      subTotal: producto.precio,
       productoId: producto.id,
       titulo: producto.denominacion,
       descripcion: producto.descripcion,
@@ -50,14 +46,14 @@ const ProductosList = ({detalleProducto,setDetalleProducto,total,setTotal,countP
     const updatedProducts = [...detalleProducto, detalleProductoItem];
     setDetalleProducto(updatedProducts);
     setCountProducts(countProducts+1);
-    setTotal(total+(detalleProductoItem.cantidad*producto.precioVenta))
+    setTotal(total+(detalleProductoItem.cantidad*producto.precio))
   };
 
   //useEffect para obtener lista de productos
 
   useEffect(() => {
     const fetchProductos = async () => {
-      const productoList = await ProductoService.getProductosList();
+      const productoList = await ProductoService.getProductosList(window.localStorage.getItem('token'));
       setProductos(productoList);
     };
 
@@ -87,8 +83,8 @@ const ProductosList = ({detalleProducto,setDetalleProducto,total,setTotal,countP
               <tr key={producto.id}>
                 <td>{producto.denominacion}</td>
                 <td>{producto.descripcion}</td>
-                <td>{producto.precioVenta}</td>
-                <td>{producto.rubroProducto.denominacion}</td>
+                <td>{producto.precio}</td>
+                <td>{producto.rubroProducto?.denominacion}</td>
                 <td>
                   <img
                     src={producto.urlImagen}
