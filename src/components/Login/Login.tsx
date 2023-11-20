5f5e392e15b1e7648ee536295f6eb67bbce0338e
MaaR120 <martineguren120@gmail.com>
2023-11-20 18:29:58 -0300

    abmpedido

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { Button } from "react-bootstrap";
 import { LoginModal } from "../LoginModal/LoginModal";
 
-
 import { LoginRequest } from "../../types/LoginRequest";
 
 export const Login: React.FC = () => {
@@ -12,19 +11,6 @@ export const Login: React.FC = () => {
       password: "",
     };
   };
-
-
-import { LoginRequest } from "../../types/LoginRequest";
-
-
-export const Login: React.FC = () => {
-  const initRequest = (): LoginRequest => {
-    return {
-      email: "",
-      password: "",
-    };
-  };
-
 
   const [showModal, setShowModal] = useState(false);
 
